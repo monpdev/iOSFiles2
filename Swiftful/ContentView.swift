59ce9bc70bc09012bc2a103705e07af57d0b0cb9
monpdev <35469743+monpdev@users.noreply.github.com>
2022-10-25 23:01:19 +0100

    Create ContentView.swift

---
+++ Swiftful/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by Mon P. on 24/10/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Placeholder")
+            .font(.title)
+            .fontWeight(.black)
+            .foregroundColor(Color.green)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
