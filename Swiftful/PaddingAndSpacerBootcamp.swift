2cdd92761737c516e4976b7c86d4e5eb1be5a493
monpdev <35469743+monpdev@users.noreply.github.com>
2022-11-03 23:46:54 +0000

    Create PaddingAndSpacerBootcamp.swift

---
+++ Swiftful/PaddingAndSpacerBootcamp.swift
@@ -0,0 +1,48 @@
+//
+//  PaddingAndSpacerBootcamp.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by Mon P. on 03/11/2022.
+//
+
+import SwiftUI
+
+struct PaddingAndSpacerBootcamp: View {
+    var body: some View {
+        VStack (alignment: .leading){
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+                .padding(.bottom, 20)
+            
+            Text("This is the description of what we will do on this screen. It is multiple lines and we will align the text to the leading edge.")
+        }
+        
+        .padding()
+        .padding(.vertical, 10)
+        .background(
+            Color.white
+                .cornerRadius(10)
+                .shadow(
+                    color: Color.black.opacity(0.3),
+                    radius: 10,
+                    x: 0.0, y:10
+                )
+        )
+        .padding(.horizontal, 10)
+     
+    }
+}
+        
+//        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+//            .background(Color.yellow)
+//            .padding(.all, 10)
+//            .padding(.leading, 20)
+//            .background(Color.blue)
+
+
+struct PaddingAndSpacerBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        PaddingAndSpacerBootcamp()
+    }
+}
