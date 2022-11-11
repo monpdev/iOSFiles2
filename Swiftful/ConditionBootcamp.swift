f887ddf3c29e97aed144c3c90b95a0fc21c2e269
monpdev <35469743+monpdev@users.noreply.github.com>
2022-11-11 23:27:23 +0000

    Create ConditionBootcamp.swift

---
+++ Swiftful/ConditionBootcamp.swift
@@ -0,0 +1,63 @@
+//
+//  ConditionalBootcamp.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by Mon P. on 10/11/2022.
+//
+
+import SwiftUI
+
+struct ConditionalBootcamp: View {
+    
+    @State var showCircle: Bool = false
+    @State var showRectangle: Bool = false
+    @State var isLoading: Bool = true
+    
+    var body: some View {
+        VStack(spacing: 20) {
+            
+            Button("IS LOADING: \(isLoading.description)") {
+                isLoading.toggle()
+            }
+            
+            if isLoading {
+                ProgressView()
+            }
+            
+        
+            
+//            Button("Circle Button: \(showCircle.description)") {
+//                showCircle.toggle()
+//            }
+//
+//            Button("Rectangle Button: \(showRectangle.description)") {
+//                showRectangle.toggle()
+//            }
+//
+//            if showCircle {
+//                Circle()
+//                    .frame(width: 100, height:100)
+//            }
+//
+//            if showRectangle {
+//                Rectangle()
+//                    .frame(width: 100, height: 100)
+//            }
+//
+//
+//            if showCircle && showRectangle {
+//                RoundedRectangle(cornerRadius: 25.0)
+//                    .frame(width: 200, height: 100)
+//
+//            }
+//
+//            Spacer()
+        }
+    }
+}
+
+struct ConditionalBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        ConditionalBootcamp()
+    }
+}
