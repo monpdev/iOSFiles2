2b4dbec5315e68f5e50dc1cd0d7ef546a6862633
monpdev <35469743+monpdev@users.noreply.github.com>
2023-01-12 12:41:15 +0000

    Create Introview.swift

---
+++ Swiftful/ManageUserOnboard/Introview.swift
@@ -0,0 +1,45 @@
+//
+//  IntroView.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by Mon P. on 08/01/2023.
+//
+
+import SwiftUI
+
+struct IntroView: View {
+    
+    @AppStorage("signed_in") var currentUserSignedIn:Bool = false
+    
+    var body: some View {
+        
+        ZStack {
+            //background
+            RadialGradient(
+                gradient: Gradient(colors: [Color(#colorLiteral(red: 0.1764705926, green: 0.01176470611, blue: 0.5607843399, alpha: 1)), Color(#colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1))]),
+                center: .topLeading,
+                startRadius: 5,
+                endRadius: UIScreen.main.bounds.height)
+                .ignoresSafeArea()
+            
+            //if user is signed in
+            //profile view
+            //else
+            //onboarding view
+            
+            if currentUserSignedIn {
+                ProfileView()
+                    .transition(.asymmetric(insertion: .move(edge:.bottom), removal: .move(edge: .top)))
+            } else {
+                OnboardingView()
+                    .transition(.asymmetric(insertion: .move(edge:.bottom), removal: .move(edge: .top)))
+            }
+        }
+    }
+}
+
+struct IntroView_Previews: PreviewProvider {
+    static var previews: some View {
+        IntroView()
+    }
+}
