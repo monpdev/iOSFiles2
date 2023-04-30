d4c15b76aa77ac01af8d54070b1e98a4a2c32a40
monpdev <35469743+monpdev@users.noreply.github.com>
2023-04-30 18:32:36 +0100

    Update PreferenceKeyBootcamp.swift

--- Swiftful2/SwiftfulThinkingContinuedLearning/Advanced/PreferenceKeyBootcamp/PreferenceKeyBootcamp.swift
+++ Swiftful2/SwiftfulThinkingContinuedLearning/Advanced/PreferenceKeyBootcamp/PreferenceKeyBootcamp.swift
@@ -1,100 +1,85 @@
 //
-//  ScrollViewOffsetPreferenceKeyBootcamp.swift
+//  PreferenceKeyBootcamp.swift
 //  SwiftfulThinkingAdvancedLearning
 //
-//  Created by Mon P. on 29/04/2023.
+//  Created by Mon P. on 26/04/2023.
 //
 
 import SwiftUI
 
-struct ScrollViewOffsetPreferenceKey: PreferenceKey {
+struct PreferenceKeyBootcamp: View {
     
-    static var defaultValue: CGFloat = 0
+    @State private var text: String = "Hello, world!"
     
-    static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
-        value = nextValue()
+    var body: some View {
+        NavigationView {
+            VStack {
+                SecondaryScreen(text: text)
+                    .navigationTitle("Navigation Title")
+                  //  .customTitle("New Value!!!")
+            }
+        }
+        .onPreferenceChange(CustomTitlePreferenceKey.self, perform: {
+            value in
+            self.text = value
+        })
     }
 }
+
 
 extension View {
     
-    func onScrollViewOffsetChanged(action: @escaping (_ offset: CGFloat) -> Void) -> some View {
+    func customTitle(_ text: String) -> some View {
+        
         self
-            .background(
-                GeometryReader {geo in
-                    Text("")
-                        .preference(key: ScrollViewOffsetPreferenceKey.self, value: geo.frame(in: .global).minY)
-                }
-            )
-            .onPreferenceChange(ScrollViewOffsetPreferenceKey.self, perform:{value in
-                    action(value)
-            })
+            .preference(key: CustomTitlePreferenceKey.self, value: text)
+        
     }
 }
 
 
-struct ScrollViewOffsetPreferenceKeyBootcamp: View {
+struct PreferenceKeyBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        PreferenceKeyBootcamp()
+    }
+}
+
+
+
+struct SecondaryScreen: View {
     
-    let title: String = "New title here!!!!"
-    @State private var scrollViewOffset: CGFloat = 0
+    let text: String
+    @State private var newValue: String = ""
     
     var body: some View {
-        ScrollView {
-            VStack {
-                titleLayer
-                    .opacity(Double(scrollViewOffset)/63.0)
-                    .onScrollViewOffsetChanged {value in
-                        self.scrollViewOffset = value
-                    }
-                  
-                contentLayer
-                
-                
-            }
-            .padding()
-        }
-        .overlay(Text("\(scrollViewOffset)"))
-        .overlay(navBarLayer
-            .opacity(scrollViewOffset < 40 ? 1.0 : 0.0)
-                 ,alignment: .top)
-    }
-}
-
-struct ScrollViewOffsetPreferenceKeyBootcamp_Previews: PreviewProvider {
-    static var previews: some View {
-        ScrollViewOffsetPreferenceKeyBootcamp()
-    }
-}
-
-
-extension ScrollViewOffsetPreferenceKeyBootcamp {
-    
-    private var titleLayer: some View {
-        Text(title)
-            .font(.largeTitle)
-            .fontWeight(.semibold)
-            .frame(maxWidth: .infinity, alignment: .leading)
-    }
-    
-    private var contentLayer: some View {
-        ForEach(0..<100) {_ in
-            RoundedRectangle(cornerRadius: 10)
-                .fill(Color.red.opacity(0.3))
-                .frame(width: 300, height: 200)
-        }
-    }
-    
-    private var navBarLayer: some View {
-        Text(title)
-            .font(.headline)
-            .frame(maxWidth: .infinity)
-            .frame(height: 55)
-            .background(Color.blue)
+        
+        Text(text)
+            .onAppear(perform: getDataFromDatabase)
+            .customTitle(newValue)
         
     }
     
+    func getDataFromDatabase() {
+        //download
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0){
+            self.newValue = "NEW VALUE FROM DATABASE"
+            
+        }
+    }
+}
+
+
+struct CustomTitlePreferenceKey: PreferenceKey {
+    
+    static var defaultValue: String = ""
+    
+    static func reduce(value: inout String, nextValue: () -> String) {
+        value = nextValue()
+    }
+    
 }
 
 
 
-//26.54..35.06
+
+
