e91a49bf932354dcbe6759c3ae5eb894d9ab1ea4
monpdev <35469743+monpdev@users.noreply.github.com>
2024-04-18 00:31:15 +0100

    Add files via upload

---
+++ CardApp/MasteryCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MasteryCardsApp.swift
+//  MasteryCards
+//
+//  Created by Mon P. on 01/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct MasteryCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
