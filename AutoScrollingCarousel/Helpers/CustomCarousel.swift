ed8b9527e9d2c2183a8180c652d6aa164a8e7c54
monpdev <35469743+monpdev@users.noreply.github.com>
2025-07-28 23:34:54 +0300

    Create CustomCarousel.swift

---
+++ AutoScrollingCarousel/Helpers/CustomCarousel.swift
@@ -0,0 +1,58 @@
+
+//
+//  CustomCarousel.swift
+//  AutoScrollingCarousel
+//
+//  Created by Mon P. on 27/07/2025.
+//
+
+import SwiftUI
+
+struct CustomCarousel<Content: View>: View {
+    
+    @ViewBuilder var content: Content
+    
+    ///View Properties
+    
+    @State private var scrollPosition: Int?
+    @State private var isScrolling: Bool = false
+    
+    var body: some View {
+        GeometryReader {
+            let size = $0.size
+            
+            ScrollView(.horizontal) {
+                HStack(spacing:0) {
+                    Group(subviews: content) {collection in
+                        if let lastItem = collection.last{
+                            lastItem
+                                .frame(width: size.width, height: size.height)
+                                .id(-1)
+                        }
+                        
+                        ForEach(collection.indices, id:\.self) {index in
+                            collection[index]
+                                .frame(width: size.width, height: size.height)
+                                .id(index)
+                            
+                        }
+                        
+                        if let firstItem = collection.first{
+                            firstItem
+                                .frame(width: size.width, height: size.height)
+                                .id(collection.count)
+                        }
+                    }
+                }
+            }
+            .scrollPosition(id: $scrollPosition)
+            .scrollTargetBehavior(.paging)
+            .scrollIndicators(.hidden)
+            .onAppear { scrollPosition = 0}
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
