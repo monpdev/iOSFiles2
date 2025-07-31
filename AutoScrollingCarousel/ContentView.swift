71620fec9ec4afc8d110855880ebf1199e6f7b52
monpdev <35469743+monpdev@users.noreply.github.com>
2025-08-01 00:01:34 +0300

    Create ContentView.swift

---
+++ AutoScrollingCarousel/ContentView.swift
@@ -0,0 +1,71 @@
+//  ContentView.swift
+//  AutoScrollingCarousel
+//
+//  Created by Mon P. on 27/07/2025.
+//
+
+import SwiftUI
+
+//Sample Model
+
+struct Item: Identifiable {
+    var id: String = UUID().uuidString
+    var color: Color
+}
+
+var mockItems: [Item] = [
+    .init(color: .orange),
+    .init(color: .blue),
+    .init(color: .green),
+    .init(color: .yellow),
+    .init(color: .red)
+]
+
+
+struct ContentView: View {
+    @State private var activePage: Int = 0
+    var body: some View {
+        NavigationStack {
+            VStack (spacing: 15) {
+                CustomCarousel (activeIndex: $activePage) {
+                    ForEach(mockItems) {item in
+                        RoundedRectangle(cornerRadius: 15)
+                            .fill(item.color.gradient)
+                            .padding(.horizontal, 15)
+                    }
+                }
+                .frame(height: 220)
+                
+                //Custom Indicator
+                HStack(spacing: 5) {
+                    ForEach(mockItems.indices, id: \.self) {index in
+                        Circle()
+                            .fill(activePage == index ? .primary : .secondary)
+                            .frame(width: 8, height: 8)
+                        
+                    }
+                }
+                .animation(.snappy, value: activePage)
+                
+            }
+            .navigationTitle("Auto Scrolling Carousel")
+            
+        }
+        
+    }
+}
+
+
+#Preview {
+    ContentView()
+}
+
+
+
+
+
+
+
+
+
+
