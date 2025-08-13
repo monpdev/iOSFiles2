eba679c53f8d3e04d89568f545046365764e94c3
monpdev <35469743+monpdev@users.noreply.github.com>
2025-08-13 18:11:52 +0300

    Update Home.swift

--- DescriptionCarousel/View/Home.swift
+++ DescriptionCarousel/View/Home.swift
@@ -1,166 +1,60 @@
 //
-//  Home.swift
+//  CardView.swift
 //  DescriptionCarousel
 //
-//  Created by Mon P. on 07/08/2025.
+//  Created by Mon P. on 11/08/2025.
 //
 
 import SwiftUI
 
-//Define width
-var width = UIScreen.main.bounds.width
-
-
-struct Home: View {
-    @EnvironmentObject var model: CarouselViewModel
-    @Namespace var animation
-    
+struct CardView: View {
+    @EnvironmentObject var model : CarouselViewModel
+    var card: Card
+    var animation: Namespace.ID
     var body: some View {
-        
         VStack {
+            
+            Text("Monday 11 August")
+                .font(.caption)
+                .foregroundColor(Color.white.opacity(0.85))
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding()
+                .padding(.top, 10)
+            
+            HStack {
+                Text(card.title)
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                    .frame(width:250, alignment: .leading)
+                    .padding()
+           
+                Spacer(minLength: 0)
+            }
+            
+            Spacer(minLength: 0)
+            
             HStack {
                 
-                Button(action: {} , label: {
-                    Image(systemName: "xmark")
-                        .font(.title2)
-                        .foregroundColor(.gray)
-                })
+                Spacer(minLength: 0)
                 
-                Text("Your Wellbeing")
-                    .fontWeight(.bold)
-                    .foregroundColor(.black)
-                    .padding(.leading)
+                Text("Read More")
                 
-                Spacer()
+                     Image(systemName: "arrow.right")
             }
-            .padding()
-            
-            //Carousel...
-            
-            ZStack {
-                ForEach(model.cards.indices.reversed(), id:\.self){index in
-                    HStack {
-                        CardView(card: model.cards[index], animation: animation)
-                            .frame(width: getCardWidth(index: index),height: getCardHeight(index: index))
-                            .offset(x: getCardOffset(index: index))
-                            .rotationEffect(.init(degrees: getCardRotation(index: index)))
-                        
-                        Spacer(minLength: 0)
-                    }
-                    .frame(height: 400)
-                    .contentShape(Rectangle())
-                    .offset(x: model.cards[index].offset)
-                    .gesture(DragGesture(minimumDistance: 0)
-                        .onChanged({ (value) in
-                            onChanged(value: value, index: index)
-                        }).onEnded({(value) in
-                            onEnd(value: value, index: index)
-                        }))
-                }
-            }
-            
-            .padding(.top, 25)
-            .padding(.horizontal,30)
-            
-            Button(action: ResetViews, label: {
-                
-                VStack {
-                    Image(systemName: "arrow.left")
-                        .font(.system(size: 20, weight: .semibold))
-                        .foregroundColor(.blue)
-                        .padding()
-                        .background(Color.white)
-                        .clipShape(Circle())
-                        .shadow(radius: 3)
-                    Text("reset")
-                }
-               
-                
-            })
-            .padding(.top, 35)
-            
-            
-            Spacer()
+            .foregroundColor(Color.white.opacity(0.9))
+            .padding(30)
+           
         }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(card.cardColor.cornerRadius(25))
+        .cornerRadius(25)
     }
-    
-    //Reseting Views
-    
-    func ResetViews() {
-        
-        for index in model.cards.indices {
-            withAnimation(.spring()){
-                
-                model.cards[index].offset = 0
-                model.swipedCard = 0
-            }
-        }
-    }
-    
-    
-    func onChanged(value: DragGesture.Value,index: Int) {
-        
-        //Only Left Swipe
-        
-        if value.translation.width < 0 {
-            model.cards[index].offset = value.translation.width
-        }
-        
-    }
-    
-    func onEnd(value: DragGesture.Value,index: Int) {
-        
-        withAnimation {
-            if -value.translation.width > width / 3{
-                model.cards[index].offset = -width
-                model.swipedCard += 1
-            }
-            else {
-                model.cards[index].offset = 0
-            }
-        }
-    }
-    
-    
-    
-    //Rotation When Card is Swiped
-    func getCardRotation(index: Int)->Double{
-        let boxWidth = Double(width / 3)
-        let offset = Double(model.cards[index].offset)
-        let angle: Double = 5
-        return (offset/boxWidth)*angle
-    }
-    
-    
-    //Getting Width And Height For Card.
-    func getCardHeight(index: Int)->CGFloat{
-        
-        let height : CGFloat = 400
-        // Do first 3 cards initially
-        let cardHeight = index - model.swipedCard <= 2 ? CGFloat(index) * 35 : 70
-        return height - cardHeight
-    }
-    
-    func getCardWidth(index: Int)->CGFloat{
-        let boxWidth = UIScreen.main.bounds.width - 60 - 60
-        
-        //For First Three cards
-      //  let cardWidth = index <= 2 ? CGFloat(index) * 30 : 60
-        
-        return boxWidth
-        
-    }
-    
-    //Get Offset
-    func getCardOffset(index: Int)->CGFloat{
-        return index - model.swipedCard  <= 2 ? CGFloat(index) * 30 : 60
-    }
-    
 }
 
 #Preview {
-    ContentView ()
+    ContentView()
 }
 
 
-//now at 14.19/21.36
+
