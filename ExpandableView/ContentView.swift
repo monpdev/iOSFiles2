2238a8093e74201608659cc0bf6c3e18f73ad49b
monpdev <35469743+monpdev@users.noreply.github.com>
2025-09-01 02:51:18 +0300

    Create ContentView.swift

---
+++ ExpandableView/ContentView.swift
@@ -0,0 +1,82 @@
+/  ExpandableView.swift
+//  ExpandView
+//
+//  Created by Mon P. on 30/08/2025.
+//
+
+import SwiftUI
+
+struct ExpandableView: View {
+    @Namespace private var namespace
+    @State private var show = false
+    
+    var thumbnail: ThumbnailView
+    var expanded: ExpandedView
+    
+    var body: some View {
+        ZStack {
+            if !show {
+                thumbnailView()
+            } else {
+                expandedView()
+            }
+        }
+        .onTapGesture {
+            if !show {
+                withAnimation(.spring(response: 0.6, dampingFraction: 0.8)){
+                    show.toggle()
+                    
+                }
+            }
+        }
+    }
+    
+    @ViewBuilder
+    private func thumbnailView() -> some View {
+        ZStack {
+            thumbnail
+                .matchedGeometryEffect(id: "view", in: namespace)
+        }
+        .background(
+            Color.gray.opacity(0.7)
+                .matchedGeometryEffect(id: "background", in: namespace)
+            
+        )
+        .mask {
+            RoundedRectangle(cornerRadius: 20, style: .continuous)
+                .matchedGeometryEffect(id: "mask", in: namespace)
+        }
+        
+    }
+    
+    @ViewBuilder
+    private func expandedView() -> some View {
+        ZStack {
+            expanded
+                .matchedGeometryEffect(id: "view", in: namespace)
+                .background(
+                    Color.gray
+                        .matchedGeometryEffect(id: "background", in: namespace)
+            )
+                .mask {
+                    RoundedRectangle(cornerRadius: 20, style: .continuous)
+                        .matchedGeometryEffect(id: "mask", in: namespace)
+                }
+                
+            Button (action: {
+                withAnimation(.spring(response: 0.6, dampingFraction: 0.8)){
+                    show.toggle()
+                }
+                
+            }, label: {
+                Image(systemName: "xmark")
+                    .foregroundStyle(.white)
+            })
+            .padding()
+            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topTrailing)
+            .matchedGeometryEffect(id: "mask", in: namespace)
+        }
+    }
+}
+
+
