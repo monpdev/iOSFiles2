bc32061535913e7452cfa83de1dd5a3f03a659ce
monpdev <35469743+monpdev@users.noreply.github.com>
2025-09-04 01:56:06 +0300

    Create ContentView.swift

---
+++ heroAnimation/ContentView.swift
@@ -0,0 +1,16 @@
+//
+//  ContentView.swift
+//  heroAnimation
+//
+//  Created by Mon P. on 01/09/2025.
+//
+
+import SwiftUI
+
+//MARK: -Sample Data
+struct CardData: Identifiable {
+    let id = UUID()
+    let title: String
+    let color: Color
+    let imageName: String
+    
