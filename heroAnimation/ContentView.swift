5438c9fed44a9f5c66656aab689495cb8c641b1f
monpdev <35469743+monpdev@users.noreply.github.com>
2025-09-05 01:19:13 +0300

    Update ContentView.swift

--- heroAnimation/ContentView.swift
+++ heroAnimation/ContentView.swift
@@ -1,4 +1,3 @@
-//
 //  ContentView.swift
 //  heroAnimation
 //
@@ -14,3 +13,29 @@ struct CardData: Identifiable {
     let color: Color
     let imageName: String
     
+}
+
+//MARK: -ContentView
+
+struct ContentView: View {
+    var body: some View {
+        CardsListView()
+    }
+}
+
+
+//MARK: -CardsListView
+struct CardsListView: View {
+    @Namespace private var namespace
+    
+    //sample card data
+    
+    @State private var cards: [CardData] = [
+        CardData(title: "SwiftUI Intro", color: .blue, imageName: "scribble"),
+        CardData(title: "Combine", color: .red, imageName: "rectangle.grid.2x2.fill"),
+        CardData(title: "Matched Geometry", color: .green, imageName: "arrow.left.and.right"),
+        CardData(title: "Async", color: .orange, imageName: "clock.badge.checkmark.fill")
+    ]
+    
+    
+}
