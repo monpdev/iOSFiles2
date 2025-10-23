c8e197060d538c5f41eca30b3e3e8955cc2e165d
monpdev <35469743+monpdev@users.noreply.github.com>
2025-10-23 23:57:38 +0300

    Create Landmark.swift

---
+++ myLandmarks/Landmark.swift
@@ -0,0 +1,39 @@
+//
+//  Landmark.swift
+//  myLandmarks
+//
+//  Created by Mon P. on 22/10/2025.
+//
+
+import Foundation
+import SwiftUI
+import CoreLocation
+
+struct Landmark: Hashable, Codable, Identifiable {
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+    
+    private var imageName: String
+    var image: Image {
+        Image(imageName)
+    }
+    
+    
+    private var coordinates: Coordinates
+    
+    var locationCoordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D (
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude)
+        
+    }
+    
+    struct Coordinates: Hashable, Codable {
+        var latitude: Double
+        var longitude: Double
+    }
+    
+}
