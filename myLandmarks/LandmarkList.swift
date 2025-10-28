1bfad49f0874b7a7f1147cacda9abef9fe52c61c
monpdev <35469743+monpdev@users.noreply.github.com>
2025-10-29 01:28:35 +0200

    Create LandmarkList.swift

---
+++ myLandmarks/LandmarkList.swift
@@ -0,0 +1,55 @@
+//
+//  LandmarkList.swift
+//  myLandmarks
+//
+//  Created by Mon P. on 23/10/2025.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    
+    @Environment(ModelData.self) var modelData
+    @State private var showFavoritesOnly = false
+    
+    var filteredLandmarks: [Landmark] {
+        modelData.landmarks.filter {landmark in
+            (!showFavoritesOnly || landmark.isFavorite)
+            
+        }
+    }
+    
+    var body: some View {
+       
+        NavigationSplitView {
+            List {
+                
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Favorites listed")
+                }
+                
+                
+                ForEach(filteredLandmarks) { landmark in
+                    NavigationLink {
+                        LandmarkDetail(landmark: landmark)
+                    } label: {
+                        LandmarkRow(landmark: landmark)
+                    }
+                }
+            }
+            .animation(.default, value: filteredLandmarks)
+            .navigationTitle("Landmarks")
+        } detail: {
+            Text("Select a Landmark")
+        }
+    }
+}
+
+
+#Preview {
+    LandmarkList()
+}
+
+
+
+
